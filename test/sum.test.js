e00be546ebf3299a71502a5dc5cd717f16a4a08b
so-brl <sonia.brunel@le-campus-numerique.fr>
2020-08-26 15:04:48 +0200

    ADD [TEST] Sum

--- test/sum.test.js
+++ test/sum.test.js
@@ -1,1 +1,18 @@
 // Add the unit tests of the sum function here
+
+const sum = require('../src/sum');
+
+
+
+test('Adds 1 + 2 to equal 3', () => {
+    expect(sum(1, 2)).toBe(3);
+});
+
+
+test('Parameters is not number', () => {
+        expect(() => sum("a", 2)).toThrow(/number/);
+});
+
+
+
+

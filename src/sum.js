4b9b3d5432098029c86ff8f003a92bfe81cf60cf
so-brl <sonia.brunel@le-campus-numerique.fr>
2020-08-26 15:04:28 +0200

    ADD [FUNCTION] Sum

--- src/sum.js
+++ src/sum.js
@@ -1,1 +1,10 @@
 // Implement the sum function here
+
+function sum(a, b) {
+    if (isNaN(a) || isNaN(b)){
+        throw ('Parameter must be a number');
+    }
+    return a + b;
+}
+module.exports = sum;
+

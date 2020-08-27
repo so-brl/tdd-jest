4ae56c8ba59069873e1bf43d0577b77814260fff
so-brl <sonia.brunel@le-campus-numerique.fr>
2020-08-27 11:24:15 +0200

    UPD [TEST] function fuzzBuzz

--- test/fizzBuzz.test.js
+++ test/fizzBuzz.test.js
@@ -3,26 +3,20 @@
 const fizzBuzzResult = require('../src/fizzBuzz');
 
 
-
 test('Fizz', () => {
         expect(fizzBuzzResult(3)).toEqual('Fizz');
-   // expect(() =>fizzBuzz(99, 2)).toThrow(/Fizz/);
     });
 
 test('Buzz', () => {
     expect(fizzBuzzResult(5)).toEqual('Buzz');
-    //expect(() =>fizzBuzz(99, 5)).toThrow(/Buzz/);
 });
 
 test('FizzBuzz', () => {
     expect( fizzBuzzResult(15)).toEqual('Buzz');
-    //expect(() =>fizzBuzz(99, 15)).toThrow(/FizzBuzz/);
-
 });
 
 test('not Fizz/Buzz', () => {
     expect( fizzBuzzResult(14)).toEqual(14);
-    //expect(() =>fizzBuzz(99, 14)).toThrow(14);
 });
 
 

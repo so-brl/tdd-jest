a93665da7f428bd48773488bdb1032fd5b9f6db9
so-brl <sonia.brunel@le-campus-numerique.fr>
2020-08-27 11:51:24 +0200

    UPD [TEST] function multiplies

--- src/multiplies.js
+++ src/multiplies.js
@@ -1,5 +1,9 @@
-// Implement the sum function here
-
+/**
+ * Calculate a multiplication
+ * @param a
+ * @param b
+ * @returns {number}
+ */
 function multiplies(a, b) {
     if (isNaN(a) || isNaN(b)){
         throw ('Parameter must be a number');
